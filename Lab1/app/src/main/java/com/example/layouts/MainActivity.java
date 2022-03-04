1ca2f0188444eb680add0b01b92d4d3a53a01d1b
Daniel Ben David <80395162+danielbd94@users.noreply.github.com>
2022-03-04 18:50:56 +0200

    Add files via upload

---
+++ Lab1/app/src/main/java/com/example/layouts/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.layout1);
+        //setContentView(R.layout.layout2);
+        setContentView(R.layout.layout3);
+        //setContentView(R.layout.layout4);
+    }
+}
